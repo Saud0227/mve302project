455b25accb726bfd3c62024e64e73875202de9ea
Carl Johan Ståhl <carljohan.p.stahl@gmail.com>
2026-04-23 23:50:20 +0200

    Worked on terminal run

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import numpy as np
+import os
+from queue import Queue, Empty
+from threading import Thread
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from structs.lilypad import CircleLilypad, TriangleLilypad, Cluster, AnyLilypad
@@ -77,19 +80,127 @@ def run_approximation(start, step, base_steps, predict_steps, size, multi_count=
             f"linear prediction: {data_item['linear']} (error: {linear_error}),"
             f"quadratic prediction: {data_item['quadratic']} (error: {quadratic_error})")
 
+_input_queue = Queue()
+
+def _stdin_listener():
+    while True:
+        try:
+            _input_queue.put(input().strip().lower())
+        except EOFError:
+            break
+
+
+def start_input_listener():
+    t = Thread(target=_stdin_listener, daemon=True)
+    t.start()
+
+
+def stop_requested() -> bool:
+    global input_queue
+    pressed_stop = False
+    while True:
+        try:
+            cmd = _input_queue.get_nowait()
+        except Empty:
+            break
+        if cmd == "c":
+            pressed_stop = True
+    return pressed_stop
+
+
+def terminal_run():
+    print("Welcome to the Pond Simulation!")
+    side_length = float(input("Enter the side length of the pond: "))
+    lilypad_type = input("Enter the type of lilypad (circle/triangle): ").strip().lower()
+    if lilypad_type == "circle":
+        lilypad_class = CircleLilypad
+    elif lilypad_type == "triangle":
+        lilypad_class = TriangleLilypad
+    else:
+        print("Invalid lilypad type, defaulting to circle.")
+        lilypad_class = CircleLilypad
+
+    pond_type_input = input("Enter the type of pond (regular/coverage): ").strip().lower()
+    if pond_type_input == "regular":
+        pond_type = Pond
+    elif pond_type_input == "coverage":
+        pond_type = CoveragePond
+    else:
+        print("Invalid pond type, defaulting to regular.")
+        pond_type = Pond
+
+    if pond_type == CoveragePond and lilypad_class == TriangleLilypad:
+        print("Triangle lilypads are not supported in coverage ponds. Defaulting to circle lilypads.")
+        lilypad_class = CircleLilypad
+
+    pop_size = int(input("Enter the number of simulations to run. If infinite, enter 0: "))
+
+    if pop_size != 0:
+        data_item = {side_length: {"data": []}}
+        run_multiple(data_item, pop_size, lilypad_class=lilypad_class, pond_type=pond_type)
+        print(f"Average number of lilypads needed to connect edges: {len(data_item["data"])}")
+        return
+
+    file_name = f"data_{'circle' if lilypad_class == CircleLilypad else 'triangle'}{'' if pond_type == Pond else '_full'}.txt"
+
+    if not os.path.exists(file_name):
+        with open(file_name, "w") as _:
+            pass
+    if os.path.getsize(file_name) > 100 * 1024 * 102:
+        base_name, ext = os.path.splitext(file_name)
+        num = 1
+        while os.path.exists(f"{base_name}_{num}{ext}"):
+            num += 1
+        file_name = f"{base_name}_{num}{ext}"
+
+    print(f"Running simulation with infinite population size. Data will be saved to {file_name}")
+    pop_size = int(input(f"How many runs before saving data: (NOTE: Larger numbers should have less runs before saving) "))
+    print("Press C to stop the simulation. It will exit when next saving point is reached. ctrl-c  will exit immediately without saving.")
+    start_input_listener()
+
+    # if data.txt doesn't exist, create it, no content
+
+
+    while True:
+        data_item = {side_length: {"data": []}}
+        run_multiple(data_item, pop_size, lilypad_class=lilypad_class, pond_type=pond_type)
+        with open(file_name, "a") as f:
+            for data_point in data_item[side_length]["data"]:
+                f.write(f"{side_length}:{data_point}\n")
+        # if file is larger than 100mb, create a new file with an incremented number at the end
+        if os.path.getsize(file_name) > 100 * 1024 * 102:
+            base_name, ext = os.path.splitext(file_name)
+            num = 1
+            while os.path.exists(f"{base_name}_{num}{ext}"):
+                num += 1
+            file_name = f"{base_name}_{num}{ext}"
+            print(f"File size exceeded 100mb, switching to new file: {file_name}")
+
+        if stop_requested():
+            print("Stopping simulation...")
+            break
+
+
+
+
+
+
+
 
 def main():
     Pond.cluster = Cluster
 
-    print(f"Running circle lilypads in regular pond...")
-    run_approximation(start=10, step=5, base_steps=5, predict_steps=3, size=100, multi_count=10,
-                      lilypad_class=CircleLilypad, pond_type=Pond)
-    print(f"\nRunning triangle lilypads in regular pond...")
-    run_approximation(start=10, step=5, base_steps=5, predict_steps=3, size=100, multi_count=10,
-                      lilypad_class=TriangleLilypad, pond_type=Pond)
-    print(f"\nRunning circle lilypads in coverage pond...")
-    run_approximation(start=10, step=5, base_steps=5, predict_steps=3, size=100, multi_count=10,
-                      lilypad_class=CircleLilypad, pond_type=CoveragePond)
+    terminal_run()
+
+    # print(f"Running circle lilypads in regular pond...")
+    # run_approximation(start=10, step=5, base_steps=5, predict_steps=3, size=100, multi_count=10,
+    #                   lilypad_class=CircleLilypad, pond_type=Pond)
+    # print(f"\nRunning triangle lilypads in regular pond...")
+    # run_approximation(start=10, step=5, base_steps=5, predict_steps=3, size=100, multi_count=10,
+    #                   lilypad_class=TriangleLilypad, pond_type=Pond)
+    # print(f"\nRunning circle lilypads in coverage pond...")
+    # run_approximation(start=10, step=5, base_steps=5, predict_steps=3, size=100, multi_count=10,
+    #                   lilypad_class=CircleLilypad, pond_type=CoveragePond)
 
 
 if __name__ == "__main__":
